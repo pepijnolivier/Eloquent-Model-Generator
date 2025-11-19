f32c136fb308f3715f1d45ccee134e89dfa9e4c9
Pepijn Olivier <olivier.pepijn@gmail.com>
2025-11-19 16:01:17 +0100

    create initial version of legacy naming straty

---
+++ src/NamingStrategies/LegacyNamingStrategy.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Pepijnolivier\EloquentModelGenerator\NamingStrategies;
+
+use Illuminate\Support\Str;
+use KitLoong\MigrationsGenerator\Schema\Schema;
+use Pepijnolivier\EloquentModelGenerator\Contracts\NamingStrategyInterface;
+use Pepijnolivier\EloquentModelGenerator\Relations\SchemaRelations;
+use Pepijnolivier\EloquentModelGenerator\Relations\ValueObjects\BelongsToManyRelationVO;
+use Pepijnolivier\EloquentModelGenerator\Relations\ValueObjects\BelongsToRelationVO;
+use Pepijnolivier\EloquentModelGenerator\Relations\ValueObjects\HasManyRelationVO;
+use Pepijnolivier\EloquentModelGenerator\Relations\ValueObjects\HasOneRelationVO;
+
+class LegacyNamingStrategy implements NamingStrategyInterface
+{
+    public static function generateModelNameFromTableName(Schema $schema, SchemaRelations $relations, string $tableName): string
+    {
+        return ucfirst(Str::camel(Str::singular($tableName)));
+    }
+
+    public static function generateHasOneFunctionName(HasOneRelationVO $vo): string
+    {
+        $hasOneModel = self::generateModelNameFromTableName(
+            $vo->getSchema(),
+            $vo->getRelations(),
+            $vo->getForeignKey()->getTableName()
+        );
+
+        return self::getSingularFunctionName($hasOneModel);
+    }
+
+    public static function generateHasManyFunctionName(HasManyRelationVO $vo): string
+    {
+        // TODO: Implement generateHasManyFunctionName() method.
+    }
+
+    public static function generateBelongsToFunctionName(BelongsToRelationVO $vo): string
+    {
+        // TODO: Implement generateBelongsToFunctionName() method.
+    }
+
+    public static function generateBelongsToManyFunctionName(BelongsToManyRelationVO $vo): string
+    {
+        // TODO: Implement generateBelongsToManyFunctionName() method.
+    }
+
+    protected static function getPluralFunctionName($modelName): string
+    {
+        $modelName = lcfirst($modelName);
+        return Str::plural($modelName);
+    }
+
+    protected static function getSingularFunctionName($modelName): string
+    {
+        $modelName = lcfirst($modelName);
+        return Str::singular($modelName);
+    }
+}
