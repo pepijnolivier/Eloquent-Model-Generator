a6d7e7e07fc82895cd04292ab2f1c4500f3f3eae
Pepijn Olivier <olivier.pepijn@gmail.com>
2025-11-25 10:28:06 +0100

    1/x belongsToMany implementation

--- src/NamingStrategies/ColumnBasedNamingStrategy.php
+++ src/NamingStrategies/ColumnBasedNamingStrategy.php
@@ -56,7 +56,6 @@ class ColumnBasedNamingStrategy implements NamingStrategyInterface
 
     public static function generateBelongsToManyFunctionName(BelongsToManyRelationVO $vo): string
     {
-
         // eg for table 'users', we have a belongsToMany 'posts' via 'comments' table
         // so the legacy name for this relation would be 'posts'
         // but we want to reflect the pivot table 'comments' in the name
@@ -66,13 +65,33 @@ class ColumnBasedNamingStrategy implements NamingStrategyInterface
         // if it's a standard pivot table, it will be the concatenation of the two related tables in alphabetical order
         // if so, we should fallback to the legacy naming strategy
 
+        $fk1 = $vo->getFk1(); // FK to the current model
+        $fk2 = $vo->getFk2(); // FK to the related model
+
+        $fk1Table = $fk1->getTableName();
+        $fk2Table = $fk2->getForeignTableName();
+
+        // dd($fk1Table, $fk2Table, $fk1->getForeignTableName());
+
+        $isStandardPivot = self::isStandardPivotTable(
+            pivotTable: $fk1Table,
+            modelTable: $fk2Table,
+            relatedTable: $fk1->getForeignTableName()
+        );
+
+        if($isStandardPivot) {
+            return LegacyNamingStrategy::generateBelongsToManyFunctionName($vo);
+        }
+
+
 
         // if it's not a standard pivot table, eg "users > comments > posts"
         // and we are processing the users table
         // then we can use the pivot table name 'comments' as a prefix for the relation
         // so the relation name becomes 'commentedPosts'
-
         return LegacyNamingStrategy::generateBelongsToManyFunctionName($vo);
+
+
     }
 
     protected static function getPluralFunctionName(string $modelName): string
@@ -85,5 +104,18 @@ class ColumnBasedNamingStrategy implements NamingStrategyInterface
     {
         $modelName = lcfirst($modelName);
         return Str::singular($modelName);
+    }
+
+    protected static function isStandardPivotTable(string $pivotTable, string $modelTable, string $relatedTable): bool
+    {
+        $singularModelTable = Str::singular($modelTable);
+        $singularRelatedTable = Str::singular($relatedTable);
+
+        $candidates = [
+            $singularModelTable . '_' . $singularRelatedTable,
+            $singularRelatedTable . '_' . $singularModelTable,
+        ];
+
+        return in_array($pivotTable, $candidates);
     }
 }
